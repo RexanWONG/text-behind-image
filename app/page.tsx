b405bce55c69945cc314bdf444ed2ea2d239ab2e
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2024-09-21 14:32:00 +0800

    start landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
 'use client'
 
 import React from 'react';
-import { useUser } from '@/hooks/useUser';
-import { useSessionContext } from '@supabase/auth-helpers-react';
-
-import Authenticate from '@/components/authenticate';
 
 const page = () => {
-    const { user } = useUser()
-    const { session } = useSessionContext()
-
     return (
-        <> 
-            {user && session && session.user ? (
-                <div className='flex flex-col min-h-screen'>
-                    Hi 
-                </div>
-            ) : (
-                <Authenticate />
-            )}
-        </>
+        <div className='flex flex-col min-h-screen'>
+            Landing page 
+        </div>
     );
 }
 
