5a84d5bb94e7a3a421710895f0158bfeee779c4f
Noodles <yilangcharleshe@gmail.com>
2024-10-17 23:56:23 -0400

    center nav

--- components/ui/animated-tab.tsx
+++ components/ui/animated-tab.tsx
@@ -43,7 +43,7 @@ export const Tabs = ({
     <>
       <div
         className={cn(
-          "flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
+          "flex flex-row items-center justify-center [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
           containerClassName
         )}
       >
