99afb559ac8ae6d06a82e68eb74985ba220524ea
Noodles <yilangcharleshe@gmail.com>
2024-10-18 18:09:28 -0400

    add images

--- components/hero-gradient-parallax-images.tsx
+++ components/hero-gradient-parallax-images.tsx
@@ -6,11 +6,13 @@ import mamba from '@/public/mamba.png'
 import mambaGradient from '@/public/mamba-gradient.png'
 import mambaGradient2 from '@/public/mamba-gradient-2.png'
 import iphone1 from '@/public/iphone-1.png'
+import flower from '@/public/vibe.png'
+import vogue from '@/public/vogue.png'
 
 export function HeroGradientParallaxImages() {
   return <ParallaxScroll images={images} className="w-full"/>;
 }
 
 const images = [
-  mamba, mambaGradient, mambaGradient2, iphone1
+  mamba, mambaGradient, mambaGradient2, iphone1, flower, vogue
 ];
