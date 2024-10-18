d797db3a13f1e6e6415e4fb406fb24b87a3b424e
Noodles <yilangcharleshe@gmail.com>
2024-10-18 00:51:15 -0400

    improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,17 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Background Magic",
-  description: "Create text behind image designs",
+  title: "Background Magics | Make Designs with AI",
+  description: "Create stunning text behind image, gradient background designs with Background Magics. Easy-to-use tool for unique visual content.",
+  keywords: "background magics, text behind image, gradient background, gradient behind image, image design, graphic design, visual content creation",
+  openGraph: {
+    title: "Background Magics | Create Text Behind Image Designs",
+    description: "Create stunning text behind image designs with Background Magic. Easy-to-use tool for unique visual content.",
+    type: "website",
+    url: "https://www.bgmagics.com", // Replace with your actual URL
+    images: [
+    ],
+  },
 };
 
 export default function RootLayout({
