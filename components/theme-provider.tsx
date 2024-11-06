87208a850166d6186e61571d64cf5dbcc7a82032
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2024-11-07 07:51:01 +0800

    add dark mode

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
