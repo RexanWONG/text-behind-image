0a8b5fd75563f464cd684ea0ccbb8215eae9c499
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2024-12-21 23:13:54 +0800

    update stats

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const page = () => {
             </HeroHighlight>
             
             <div className="text-lg text-center font-semibold mb-4">
-                150,000+ text behind image designs created
+                200,000+ text behind image designs created
             </div>
 
             <Link href={'/app'} className='mb-10'>
