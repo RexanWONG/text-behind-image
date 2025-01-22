c7eb6088a7d22713b9f73ee81427b9a76d369d49
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2025-01-22 22:44:41 +0800

    try change file name

--- components/editor/app-ads.tsx
+++ components/editor/app-ads.tsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react'
 import { X } from 'lucide-react'
 import Image, { StaticImageData } from 'next/image'
 import formfactorImg from '@/public/ads/formfactor.jpg'
-import ninjatoolsImg from '@/public/ads/ninjatools.png'
- 
+import ninjatoolsImg from '../../public/ads/ninjatools.png'
+
 interface Ad {
     name: string;
     image: StaticImageData;
